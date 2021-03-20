1f953aa60016cb1da4a7fed88f8d5168e5956cfa
Lucas <lucasvalenteds@gmail.com>
2021-03-20 15:21:39 -0300

    Configuring Jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,9 @@ configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.example.Main"
+    }
+}
